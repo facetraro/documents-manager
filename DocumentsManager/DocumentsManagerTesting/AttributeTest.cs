78d96d37f3284a1234f7598a2ef96972d4d12bb3
Federico Cetraro <facetraro@gmail.com>
2018-04-20 15:15:43 -0300

    [RED] AttributeEquals

---
+++ DocumentsManager/DocumentsManagerTesting/AttributeTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DocumentsMangerEntities;
+
+namespace DocumentsManagerTesting
+{
+
+    [TestClass]
+    public class AttributeTest
+    {
+        [TestMethod]
+        public void AttributeEquals()
+        {
+            DocumentsMangerEntities.Attribute fontSizeTest = new FontSize();
+            fontSizeTest.Equals(fontSizeTest);
+        }
+    }
+}
