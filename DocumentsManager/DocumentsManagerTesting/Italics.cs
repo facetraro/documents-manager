ee88743f4270b02a4c2c6391636798ca61847b48
Federico Cetraro <facetraro@gmail.com>
2018-04-20 15:49:06 -0300

    [GREEN] StyleAttributeItalicsConstructor

---
+++ DocumentsManager/DocumentsManagerTesting/Italics.cs
@@ -0,0 +1,12 @@
+﻿using DocumentsMangerEntities;
+
+namespace DocumentsManagerTesting
+{
+    internal class Italics : StyleAttribute
+    {
+        public Italics()
+        {
+            Name = "Cursiva";
+        }
+    }
+}
