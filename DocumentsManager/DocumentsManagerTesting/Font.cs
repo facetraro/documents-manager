06f30e19e5cc68b4b84d64e79e9267ed351c5ed9
Federico Cetraro <facetraro@gmail.com>
2018-04-20 16:17:53 -0300

    [GREEN] StyleAttributeFontConstructor

---
+++ DocumentsManager/DocumentsManagerTesting/Font.cs
@@ -0,0 +1,12 @@
+﻿using DocumentsMangerEntities;
+
+namespace DocumentsManagerTesting
+{
+    internal class Font : StyleAttribute
+    {
+        public Font()
+        {
+            Name = "Tipo de Letra";
+        }
+    }
+}
