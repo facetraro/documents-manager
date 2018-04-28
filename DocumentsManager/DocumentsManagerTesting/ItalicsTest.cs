7fcbd95b25ffdbae9e00b4dfddd58e18f6d29868
Federico Cetraro <facetraro@gmail.com>
2018-04-28 15:22:30 -0300

    [GREEN] EqualItalicsTest

--- DocumentsManager/DocumentsManagerTesting/ItalicsTest.cs
+++ DocumentsManager/DocumentsManagerTesting/ItalicsTest.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DocumentsMangerEntities;
 
 namespace DocumentsManagerTesting
 {
     [TestClass]
     public class ItalicsTest
     {
-
         [TestMethod]
-        public void EqualBoldTest()
+        public void EqualItalicsTest()
         {
             Italics testItalics = new Italics();
             testItalics.Applies = ApplyValue.Apply;
             Assert.IsTrue(testItalics.Equals(testItalics));
         }
-       
     }
 }
