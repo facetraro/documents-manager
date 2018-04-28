ef6c5bead60fae9be034c390403a4de282c87337
Federico Cetraro <facetraro@gmail.com>
2018-04-28 13:48:13 -0300

    [RED] EqualBoldTest

---
+++ DocumentsManager/DocumentsManagerTesting/BoldTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DocumentsManagerTesting
+{
+    [TestClass]
+    public class BoldTest
+    {
+        [TestMethod]
+        public void EqualBoldTest()
+        {
+            Bold testBold = new Bold();
+            testBold.Applies = ApplyValue.Apply;
+            Assert.IsTrue(testBold.Equals(testBold));
+        }
+    }
+}
