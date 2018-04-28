31bbd20e864a9eadaee32e05d646e43fc02051d9
Federico Cetraro <facetraro@gmail.com>
2018-04-28 13:53:49 -0300

    [GREEN] NotEqualBoldTest

--- DocumentsManager/DocumentsMangerEntities/Attributes/Bold.cs
+++ DocumentsManager/DocumentsMangerEntities/Attributes/Bold.cs
@@ -9,5 +9,10 @@ namespace DocumentsManagerTesting
         {
             Name = "Negrita";
         }
+        public override bool Equals(object obj)
+        {
+            Bold anotherBold = obj as Bold;
+            return base.Equals(obj) && Applies == anotherBold.Applies;
+        }
     }
 }
