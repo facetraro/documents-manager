c3099a225d53f1d54237a471032030adb755991c
Federico Cetraro <facetraro@gmail.com>
2018-04-28 14:49:25 -0300

    [RED] NotEqualAlignmentTest

---
+++ DocumentsManager/DocumentsManagerTesting/AlignmentTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DocumentsManagerTesting
+{
+    [TestClass]
+    public class FontTest
+    {
+        [TestMethod]
+        public void EqualFontTypeTest()
+        {
+            Font testFont = new Font();
+            testFont.FontType = DocumentsMangerEntities.FontType.Arial;
+            Font anotherFont = new Font();
+            testFont.FontType = DocumentsMangerEntities.FontType.TimesNewRoman;
+            Assert.IsFalse(testFont.Equals(anotherFont));
+        }
+        [TestMethod]
+        public void NotEqualFontTypeTest()
+        {
+            Font testFont = new Font();
+            testFont.FontType = DocumentsMangerEntities.FontType.Arial;
+            Assert.IsFalse(testFont.Equals("Not a Font"));
+        }
+    }
+}
