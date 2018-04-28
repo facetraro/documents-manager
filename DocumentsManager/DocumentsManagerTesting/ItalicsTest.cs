b487521ffd664bc1f5f050db7a73be8643ba2db8
Federico Cetraro <facetraro@gmail.com>
2018-04-28 15:21:48 -0300

    [RED] EqualItalicsTest

---
+++ DocumentsManager/DocumentsManagerTesting/ItalicsTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DocumentsManagerTesting
+{
+    [TestClass]
+    public class ItalicsTest
+    {
+
+        [TestMethod]
+        public void EqualBoldTest()
+        {
+            Italics testItalics = new Italics();
+            testItalics.Applies = ApplyValue.Apply;
+            Assert.IsTrue(testItalics.Equals(testItalics));
+        }
+       
+    }
+}
