add658ef1b5998ebcaf47e95ca811604552b3429
Federico Cetraro <facetraro@gmail.com>
2018-04-28 14:10:42 -0300

    [RED] EqualColorTest

---
+++ DocumentsManager/DocumentsManagerTesting/StyleColorTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DocumentsMangerEntities;
+
+namespace DocumentsManagerTesting
+{
+    [TestClass]
+    public class StyleColorTest
+    {
+        [TestMethod]
+        public void EqualColorTest()
+        {
+            StyleColor testColor = new StyleColor();
+            testColor.TextColor = TextColor.Black;
+            Assert.IsTrue(testColor.Equals(testColor));
+        }
+    }
+}
