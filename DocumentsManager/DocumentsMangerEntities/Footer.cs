214cf292409c56dd7b4278918ed4ea325de6889e
Fabian Grobert <fabian.grobert@gmail.com>
2018-04-29 16:44:15 -0300

    [GREEN] FooterBuilder

---
+++ DocumentsManager/DocumentsMangerEntities/Footer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentsMangerEntities
+{
+    public class Footer: DocumentObject
+    {
+        public Guid Id { get; set; }
+        public Text Text { get; set; }
+
+    }
+}
