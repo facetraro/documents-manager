8a8e3d3997dd299bc32091308f4c4cf0a844add9
Federico Cetraro <facetraro@gmail.com>
2018-04-29 14:27:42 -0300

    [RED] AddEditorTest

---
+++ DocumentsManager/DocumentsManagerDATesting/EditorUserContextTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DocumentsMangerEntities;
+using System.Collections.Generic;
+using DocumentsManagerDataAccess;
+using DocumentsManagerExampleInstances;
+
+namespace DocumentsManagerDATesting
+{
+    [TestClass]
+    public class EditorUserContextTest
+    {
+        [TestMethod]
+        public void AddEditorTest()
+        {
+            EditorUserContext context = new EditorUserContext();
+            EditorUser newUser = EntitiesExampleInstances.TestEditorUser();
+            context.Add(newUser);
+            List<EditorUser> allEditors = context.GetLazy();
+            Assert.IsTrue(allEditors.Contains(newUser));
+        }
+    }
+}
