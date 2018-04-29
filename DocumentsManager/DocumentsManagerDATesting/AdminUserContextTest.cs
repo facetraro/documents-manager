2cfcac19532fef2b940bd49da8cb297349efe91b
Federico Cetraro <facetraro@gmail.com>
2018-04-28 21:07:41 -0300

    [RED] AddAdminTest

---
+++ DocumentsManager/DocumentsManagerDATesting/AdminUserContextTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DocumentsMangerEntities;
+using System.Collections.Generic;
+
+namespace DocumentsManagerDATesting
+{
+    [TestClass]
+    public class AdminUserContextTest
+    {
+        [TestMethod]
+        public void AddAdminTest()
+        {
+            AdminUser newUser = new AdminUser();
+            ContextDataAccess context = new ContextDataAccess();
+            context.Add(newUser);
+            List<AdminUser> allAdmins = context.GetLazy();
+            Assert.IsTrue(allAdmins.Count==1);
+        }
+    }
+}
