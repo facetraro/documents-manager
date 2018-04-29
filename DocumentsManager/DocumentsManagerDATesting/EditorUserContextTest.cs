3eb77ab758e28cdacbd97d1d6e6fbaa7171b5ce6
Federico Cetraro <facetraro@gmail.com>
2018-04-29 14:47:41 -0300

    [RED] ModifyEditorTest

--- DocumentsManager/DocumentsManagerDATesting/EditorUserContextTest.cs
+++ DocumentsManager/DocumentsManagerDATesting/EditorUserContextTest.cs
@@ -40,5 +40,20 @@ namespace DocumentsManagerDATesting
             Assert.IsFalse(allEditors.Contains(newUser));
             TearDown(); 
         }
+        [TestMethod]
+        public void ModifyEditorTest()
+        {
+            EditorUserContext context = new EditorUserContext();
+            EditorUser newUser = EntitiesExampleInstances.TestEditorUser();
+            context.Add(newUser);
+            newUser.Email = "newEmail";
+            newUser.Name = "newName";
+            context.Modify(newUser);
+            List<EditorUser> allEditors = context.GetLazy();
+            Assert.IsTrue(allEditors.Contains(newUser));
+            Assert.IsTrue(allEditors.Find(item => item.Id == newUser.Id).Email == newUser.Email);
+            Assert.IsTrue(allEditors.Find(item => item.Id == newUser.Id).Name == newUser.Name);
+            TearDown();
+        }
     }
 }
