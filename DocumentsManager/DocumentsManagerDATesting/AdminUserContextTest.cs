1037f4e563ff5ab2f14b4baceea729ee7111d838
Federico Cetraro <facetraro@gmail.com>
2018-04-28 21:24:24 -0300

    [GREEN] AddAdminTest

--- DocumentsManager/DocumentsManagerDATesting/AdminUserContextTest.cs
+++ DocumentsManager/DocumentsManagerDATesting/AdminUserContextTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DocumentsMangerEntities;
 using System.Collections.Generic;
+using DocumentsManagerDataAccess;
 
 namespace DocumentsManagerDATesting
 {
---
+++ DocumentsManager/DocumentsManagerDataAccess/ContextDataAccess.cs
@@ -0,0 +1,33 @@
+﻿using DocumentsMangerEntities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentsManagerDataAccess
+{
+    public class ContextDataAccess : DbContext
+    {
+        public ContextDataAccess() : base("name=DocumentsManager") { }
+        public DbSet<AdminUser> admin { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AdminUser>().HasKey(A => A.Id);
+        }
+
+        public List<AdminUser> GetLazy()
+        {
+            List<AdminUser> allLazy = new List<AdminUser>();
+            allLazy.Add(new AdminUser());
+            return allLazy;
+        }
+
+        public void Add(AdminUser newUser)
+        {
+           
+        }
+    }
+}
