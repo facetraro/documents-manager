f7e42785557d07b9b65ddc275ccb610808c6ba7b
Federico Cetraro <facetraro@gmail.com>
2018-04-29 18:13:02 -0300

    [RED] AddStyleClassTest

---
+++ DocumentsManager/DocumentsManagerDATesting/StyleClassContextTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DocumentsMangerEntities;
+using System.Collections.Generic;
+using DocumentsManagerDataAccess;
+using DocumentsManagerExampleInstances;
+
+namespace DocumentsManagerDATesting
+{
+    [TestClass]
+    public class StyleClassContextTest
+    {
+        [TestMethod]
+        public void AddStyleClassTest()
+        {
+            StyleClassContext context = new StyleClassContext();
+            StyleClass newStyle = EntitiesExampleInstances.TestStyleClass();
+            context.Add(newStyle);
+            List<StyleClass> allStyles = context.GetLazy();
+            Assert.IsTrue(allStyles.Contains(newStyle));
+        }
+    }
+}
