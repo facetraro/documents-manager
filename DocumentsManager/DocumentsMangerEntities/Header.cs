2de55ed258ab840976a818179bc7d0ace8d0c4a4
Fabian Grobert <fabian.grobert@gmail.com>
2018-04-29 16:21:43 -0300

    [GREEN] HeaderBuilder

---
+++ DocumentsManager/DocumentsMangerEntities/Header.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentsMangerEntities
+{
+    public class Header
+    {
+        public Guid Id { get; set; }
+        public Text Text { get; set; }
+        public StyleClass StyleClass { get; set; }
+
+    }
+}
