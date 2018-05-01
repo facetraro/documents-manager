000ffe1dddfe159b3b64ab24aa80e568824a697c
Fabian Grobert <fabian.grobert@gmail.com>
2018-05-01 19:30:06 -0300

    [GREEN]RemoveById EditorUuserContextTest

---
+++ DocumentsManager/DocumentsManager.Data.DA.Handler/EditorUserContext.cs
@@ -0,0 +1,70 @@
+﻿using DocumentsManager.Data.Repository;
+using DocumentsMangerEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentsManagerDataAccess
+{
+    public class EditorUserContext
+    {
+        public List<EditorUser> GetLazy()
+        {
+            using (var db = new ContextDataAccess())
+            {
+                var unitOfWork = new UnitOfWork(db);
+                return unitOfWork.EditorRepository.Get().ToList();
+            }
+        }
+        public void ClearAll()
+        {
+            foreach (var item in GetLazy())
+            {
+                Remove(item);
+            }
+        }
+        public void Add(EditorUser newUser)
+        {
+            using (var db = new ContextDataAccess())
+            {
+                var unitOfWork = new UnitOfWork(db);
+                unitOfWork.EditorRepository.Insert(newUser);
+            }
+        }
+
+        public void Remove(EditorUser userToDelete)
+        {
+            using (var db = new ContextDataAccess())
+            {
+                var unitOfWork = new UnitOfWork(db);
+                unitOfWork.EditorRepository.Delete(userToDelete);
+            }
+        }
+        public void Remove(Guid id)
+        {
+            using (var db = new ContextDataAccess())
+            {
+                var unitOfWork = new UnitOfWork(db);
+                unitOfWork.EditorRepository.Delete(id);
+            }
+        }
+        public EditorUser GetById(Guid id)
+        {
+            using (var db = new ContextDataAccess())
+            {
+                var unitOfWork = new UnitOfWork(db);
+                return unitOfWork.EditorRepository.GetByID(id);
+            }
+        }
+        public void Modify(EditorUser modifiedUser)
+        {
+            using (var db = new ContextDataAccess())
+            {
+                var unitOfWork = new UnitOfWork(db);
+                unitOfWork.EditorRepository.Update(modifiedUser);
+            }
+        }
+    }
+}
