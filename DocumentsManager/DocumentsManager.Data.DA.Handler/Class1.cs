c4dc0fa3304f4f1fb327ad1d26ee6d68ec15eca0
Fabian Grobert <fabian.grobert@gmail.com>
2018-05-01 17:53:09 -0300

    Se crea proyecto de handler de data acces

---
+++ DocumentsManager/DocumentsManager.Data.DA.Handler/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentsManager.Data.DA.Handler
+{
+    public class Class1
+    {
+    }
+}
