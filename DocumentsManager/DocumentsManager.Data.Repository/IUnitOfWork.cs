a6a64c95c2acbe99c8845d669192c92d648fc173
Federico Cetraro <facetraro@gmail.com>
2018-05-01 17:44:25 -0300

    [REFACTOR] Se implementa Unit Of Work y se modifican nombres de los Proyectos

---
+++ DocumentsManager/DocumentsManager.Data.Repository/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using DocumentsMangerEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentsManager.Data.Repository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<AdminUser> AdminRepository { get; }
+        IRepository<EditorUser> EditorRepository { get; }
+        IRepository<StyleClass> StyleClassRepository { get; }
+        IRepository<StyleAttribute> StyleAttributeRepository { get; }
+        void Save();
+    }
+}
---
+++ DocumentsManager/DocumentsManager.Data.Repository/UnitOfWork.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentsMangerEntities;
+using DocumentsManagerDataAccess;
+
+namespace DocumentsManager.Data.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ContextDataAccess context;
+        private GenericRepository<AdminUser> adminUserRepository;
+        private GenericRepository<EditorUser> editorUserRepository;
+        private GenericRepository<StyleClass> styleClassRepository;
+        private GenericRepository<StyleAttribute> styleAttributeRepository;
+
+        public UnitOfWork(ContextDataAccess documentsManagerContext)
+        {
+            context = documentsManagerContext;
+        }
+
+        public IRepository<AdminUser> AdminRepository
+        {
+            get
+            {
+                if (this.adminUserRepository == null)
+                {
+                    this.adminUserRepository = new GenericRepository<AdminUser>(context);
+                }
+                return adminUserRepository;
+            }
+        }
+
+        public IRepository<EditorUser> EditorRepository
+        {
+            get
+            {
+                if (this.editorUserRepository == null)
+                {
+                    this.editorUserRepository = new GenericRepository<EditorUser>(context);
+                }
+                return editorUserRepository;
+            }
+        }
+
+        public IRepository<StyleClass> StyleClassRepository
+        {
+            get
+            {
+                if (this.styleClassRepository == null)
+                {
+                    this.styleClassRepository = new GenericRepository<StyleClass>(context);
+                }
+                return styleClassRepository;
+            }
+        }
+
+        public IRepository<StyleAttribute> StyleAttributeRepository
+        {
+            get
+            {
+                if (this.styleAttributeRepository == null)
+                {
+                    this.styleAttributeRepository = new GenericRepository<StyleAttribute>(context);
+                }
+                return styleAttributeRepository;
+            }
+        }
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
