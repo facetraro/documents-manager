89fd3b4a0affb1cc971d2bef94fa12d8f3a0f332
Federico Cetraro <facetraro@gmail.com>
2018-05-02 19:36:37 -0300

    [RED] AddFormatTest

---
+++ DocumentsManager/DocumentsManagerDATesting/FormatContextTest.cs
@@ -0,0 +1,34 @@
+﻿using DocumentsManager.Data.DA.Handler;
+using DocumentsManagerExampleInstances;
+using DocumentsMangerEntities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentsManagerDATesting
+{
+    [TestClass]
+    public class FormatContextTest
+    {
+        [TestMethod]
+        public void AddFormatTest()
+        {
+            FormatContext context = new FormatContext();
+            StyleClassContextHandler contextStyle = new StyleClassContextHandler();
+            Format newFormat = EntitiesExampleInstances.TestFormat();
+            StyleClass anotherStyle = EntitiesExampleInstances.TestStyleClass();
+            anotherStyle.Name = "anotherStyle";
+            newFormat.StyleClasses.Add(anotherStyle);
+            foreach (var item in newFormat.StyleClasses)
+            {
+                contextStyle.Add(item);
+            }
+            context.Add(newFormat);
+            List<Format> allFormats = context.GetLazy();
+            Assert.IsTrue(allFormats.Contains(newFormat));
+        }
+    }
+}
