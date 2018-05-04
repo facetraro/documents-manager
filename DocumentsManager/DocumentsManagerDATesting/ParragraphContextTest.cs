d844e810bc44d5730d43e66c5f544eca3ed9da77
Fabian Grobert <fabian.grobert@gmail.com>
2018-05-04 16:12:12 -0300

    [RED] AddParragraph ParragraphContextTest

---
+++ DocumentsManager/DocumentsManagerDATesting/ParragraphContextTest.cs
@@ -0,0 +1,61 @@
+﻿using DocumentsManager.Data.DA.Handler;
+using DocumentsManagerExampleInstances;
+using DocumentsMangerEntities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentsManagerDATesting
+{
+    [TestClass]
+    public class ParragraphContextTest
+    {
+        
+        [TestMethod]
+        public void AddParragraphTest()
+        {
+            ParragraphContext context = new ParragraphContext();
+            Parragraph newParragraph = EntitiesExampleInstances.TestParragraph();
+            StyleClass style = EntitiesExampleInstances.TestStyleClass();
+            Text newText = EntitiesExampleInstances.TestText();
+            StyleClassContextHandler contextsc = new StyleClassContextHandler();
+            TextContext contextT = new TextContext();
+            contextsc.Add(style);
+            newText.StyleClass = style;
+            contextT.Add(newText);
+            newParragraph.StyleClass = style;
+            newParragraph.AddText(newText);
+            context.Add(newParragraph);
+            List<Parragraph> allnewParragraphs = context.GetLazy();
+            Assert.IsTrue(allnewParragraphs.Contains(newParragraph));
+           
+        }
+        [TestMethod]
+        public void AddTwoParragraphsTest()
+        {
+            ParragraphContext context = new ParragraphContext();
+            Parragraph newParragraph = EntitiesExampleInstances.TestParragraph();
+            Parragraph sndParragraph = EntitiesExampleInstances.TestParragraph();
+            StyleClass style = EntitiesExampleInstances.TestStyleClass();
+            Text newText = EntitiesExampleInstances.TestText();
+            StyleClassContextHandler contextsc = new StyleClassContextHandler();
+            TextContext contextT = new TextContext();
+            contextsc.Add(style);
+            newText.StyleClass = style;
+            contextT.Add(newText);
+            newParragraph.StyleClass = style;
+            newParragraph.AddText(newText);
+            sndParragraph.StyleClass = style;
+            sndParragraph.AddText(newText);
+            context.Add(newParragraph);
+            context.Add(sndParragraph);
+            List<Parragraph> allParragraphs = context.GetLazy();
+            Assert.IsTrue(allParragraphs.Contains(sndParragraph) && allParragraphs.Contains(newParragraph));
+           
+        }
+       
+    }
+}
