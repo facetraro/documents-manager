2e562f17d842ce192c74fa0578b32885028bf844
Fabian Grobert <fabian.grobert@gmail.com>
2018-05-05 20:38:42 -0300

    [GREEN] GetAllEditorsOkTest

---
+++ DocumentsManager/BusinessLogic/IEditorBusinessLogic.cs
@@ -0,0 +1,18 @@
+﻿using DocumentsMangerEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentsManager.BusinessLogic
+{
+    public interface IEditorsBusinessLogic
+    {
+        IEnumerable<EditorUser> GetAllEditors();
+        EditorUser GetByID(Guid id);
+        Guid Add(EditorUser editor);
+        bool Delete(Guid id);
+        bool Update(EditorUser newBreed);
+    }
+}
---
+++ DocumentsManager/DocumentsManager.Web.Api/Controllers/EditorController.cs
@@ -0,0 +1,64 @@
+﻿using DocumentsManager.BusinessLogic;
+using DocumentsMangerEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+namespace DocumentsManager.Web.Api.Controllers
+{
+    public class EditorController : ApiController
+    {
+        private IEditorsBusinessLogic editorsBuisnessLogic { get; set; }
+
+        public EditorController(IEditorsBusinessLogic logic)
+        {
+            this.editorsBuisnessLogic = logic;
+        }
+
+        // GET: api/Editor
+        public IHttpActionResult Get()
+        {
+            IEnumerable<EditorUser> editors = editorsBuisnessLogic.GetAllEditors();
+            if (editors == null)
+            {
+                return NotFound();
+            }
+            return Ok(editors);
+        }
+
+        // GET: api/Editor/5
+        public IHttpActionResult Get(Guid id)
+        {
+            try
+            {
+                EditorUser editor = editorsBuisnessLogic.GetByID(id);
+                if (editor == null)
+                {
+                    return NotFound();
+                }
+                return Ok(editor);
+            }
+            catch (ArgumentNullException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // POST: api/Editor
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Editor/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Editor/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
