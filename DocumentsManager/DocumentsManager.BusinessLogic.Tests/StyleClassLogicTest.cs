1ee93d8837ed08ca2de52d2a7f8c25a30548b6b7
Federico Cetraro <facetraro@gmail.com>
2018-05-07 17:47:36 -0300

    [RED] GetHtmlTextEmpty

---
+++ DocumentsManager/DocumentsManager.BusinessLogic.Tests/StyleClassLogicTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DocumentsManager.BusinessLogic;
+using DocumentsMangerEntities;
+
+namespace DocumentsManager.BusinessLogic.Tests
+{
+    [TestClass]
+    public class StyleClassLogicTest
+    {
+        [TestMethod]
+        public void GetHtmlTextEmpty()
+        {
+            StyleClassBusinessLogic logic = new StyleClassBusinessLogic();
+            Assert.AreEqual("<p>Empty</p>", logic.GetHtmlText(new StyleClass(), "Empty"));
+        }
+    }
+}
