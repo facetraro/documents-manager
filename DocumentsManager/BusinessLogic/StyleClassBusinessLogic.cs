e23c8b9c0f7c9cb7e2f03743381ccee52fa95393
Federico Cetraro <facetraro@gmail.com>
2018-05-07 17:47:51 -0300

    [GREEN] GetHtmlTextEmpty

---
+++ DocumentsManager/BusinessLogic/StyleClassBusinessLogic.cs
@@ -0,0 +1,17 @@
+﻿using DocumentsMangerEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentsManager.BusinessLogic
+{
+    public class StyleClassBusinessLogic
+    {
+        public string GetHtmlText(StyleClass style, string text)
+        {
+            return $"<p>{text}</p>";
+        }
+    }
+}
