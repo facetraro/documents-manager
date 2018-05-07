d874176ff71fd049f422d551df7e1e6ea2df9004
Fabian Grobert <fabian.grobert@gmail.com>
2018-05-06 21:45:06 -0300

    [RED] Remove DocumentContext DocumentContextTest

--- DocumentsManager/DocumentsManagerDATesting/DocumentContextTest.cs
+++ DocumentsManager/DocumentsManagerDATesting/DocumentContextTest.cs
@@ -13,6 +13,10 @@ namespace DocumentsManagerDATesting
     [TestClass]
     public class DocumentContextTest
     {
+        public void TearDown()
+        {
+            ClearDataBase.ClearAll();
+        }
         [TestMethod]
         public void AddDocumentTest()
         {
@@ -36,9 +40,41 @@ namespace DocumentsManagerDATesting
             }
             newText.StyleClass = style;
             newDocument.StyleClass = style;
+            newDocument.Parragraphs.ElementAt(0).Document = newDocument;
             context.Add(newDocument);
             List<Document> allDocumentss = context.GetLazy();
             Assert.IsTrue(allDocumentss.Contains(newDocument));
+            TearDown();
+        }
+        [TestMethod]
+        public void RemoveDocumentTest()
+        {
+            DocumentContext context = new DocumentContext();
+            FormatContext contextFormat = new FormatContext();
+            Document newDocument = EntitiesExampleInstances.TestDocument();
+            StyleClass style = EntitiesExampleInstances.TestStyleClass();
+            Text newText = EntitiesExampleInstances.TestText();
+            StyleClassContextHandler contextsc = new StyleClassContextHandler();
+            contextsc.Add(style);
+            contextsc.Add(newDocument.Footer.StyleClass);
+            contextsc.Add(newDocument.Header.StyleClass);
+            foreach (var item in newDocument.Format.StyleClasses)
+            {
+                contextsc.Add(item);
+            }
+            contextFormat.Add(newDocument.Format);
+            foreach (var item in newDocument.Parragraphs)
+            {
+                contextsc.Add(item.StyleClass);
+            }
+            newText.StyleClass = style;
+            newDocument.StyleClass = style;
+            newDocument.Parragraphs.ElementAt(0).Document = newDocument;
+            context.Add(newDocument);
+            context.Remove(newDocument);
+            List<Document> allDocumentss = context.GetLazy();
+            Assert.IsFalse(allDocumentss.Contains(newDocument));
+            TearDown();
         }
     }
 }
