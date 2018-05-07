15caf512a5ee457b53de7f587aa9bededd6c7a1f
Federico Cetraro <facetraro@gmail.com>
2018-05-07 12:08:32 -0300

    [RED] AddModifyDocumentHistoryTest

---
+++ DocumentsManager/DocumentsManagerDATesting/ModifyDocumentHistoryContextTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DocumentsMangerEntities;
+using System.Collections.Generic;
+using DocumentsManagerDataAccess;
+using DocumentsManagerExampleInstances;
+using DocumentsManager.Data.DA.Handler;
+using System.Linq;
+
+namespace DocumentsManagerDATesting
+{
+    [TestClass]
+    public class ModifyDocumentHistoryContextTest
+    {
+        public void TearDown()
+        {
+            ClearDataBase.ClearAll();
+        }
+        [TestMethod]
+        public void AddModifyDocumentHistoryTest()
+        {
+            ModifyDocumentHistory newHistory = EntitiesExampleInstances.TestModifyDocumentHistory();
+            UserContext contextUser = new UserContext();
+            contextUser.Add(newHistory.User);
+            DocumentContext contextDocument = new DocumentContext();
+            contextDocument.Add(newHistory.Document);
+            ModifyDocumentHistoryContext context = new ModifyDocumentHistoryContext();
+            context.Add(newHistory);
+            List<ModifyDocumentHistory> allHistories = context.GetAllHistories();
+            Assert.IsTrue(allHistories.Contains(newHistory));
+            TearDown();
+        }
+    }
+}
