69dc0a9971e308a992c3ba2917b444980a678cb2
Federico Cetraro <facetraro@gmail.com>
2018-05-07 11:33:49 -0300

    [GREEN] ModifyDocumentHistoryBuilder

---
+++ DocumentsManager/DocumentsMangerEntities/ModifyDocumentHistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentsMangerEntities
+{
+    public class ModifyDocumentHistory
+    {
+        public Guid Id { get; set; }
+        public User User { get; set; }
+        public Document Document { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
