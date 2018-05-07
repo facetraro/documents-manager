5176e9222b9e4a09f8e25a8883c9929e5ce31e58
Federico Cetraro <facetraro@gmail.com>
2018-05-07 11:33:42 -0300

    [RED] ModifyDocumentHistoryBuilder

---
+++ DocumentsManager/DocumentsManagerTesting/ModifyDocumentHistoryTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DocumentsMangerEntities;
+using DocumentsManagerExampleInstances;
+
+namespace DocumentsManagerTesting
+{
+    [TestClass]
+    public class ModifyDocumentHistoryTest
+    {
+        [TestMethod]
+        public void ModifyDocumentHistoryBuilder()
+        {
+            ModifyDocumentHistory newHistroy = new ModifyDocumentHistory();
+            Guid newGuid = Guid.NewGuid();
+            DateTime newDate = new DateTime();
+            User newUser = EntitiesExampleInstances.TestAdminUser();
+            Document newDocument = EntitiesExampleInstances.TestDocument();
+            newHistroy.Date = newDate;
+            newHistroy.Document = newDocument;
+            newHistroy.Id = newGuid;
+            newHistroy.User = newUser;
+            Assert.AreEqual(newHistroy.User, newUser);
+            Assert.AreEqual(newHistroy.Document, newDocument);
+            Assert.AreEqual(newHistroy.Id, newGuid);
+            Assert.AreEqual(newHistroy.Date, newDate);
+        }
+    }
+}
