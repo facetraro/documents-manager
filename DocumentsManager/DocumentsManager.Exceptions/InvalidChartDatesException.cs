503e2d4da731d3d4a9fd9b41c8236bfacaa85838
Federico Cetraro <facetraro@gmail.com>
2018-05-08 12:29:46 -0300

    [GREEN] GetChartFromDocumentsTests

---
+++ DocumentsManager/DocumentsManager.Exceptions/InvalidChartDatesException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentsManager.Exceptions
+{
+    public class InvalidChartDatesException : Exception
+    {
+        private static string message = "La fecha [Desde] no puede ser despues que la fecha [Hasta]";
+        public InvalidChartDatesException() : base(message)
+        {
+        }
+    }
+}
