6bbf44122a41977a4a0fd245d7ac0d28b8a979bd
Federico Cetraro <facetraro@gmail.com>
2018-05-09 16:55:33 -0300

    [REFACTOR] GetDocumentById

--- DocumentsManager/BusinessLogic/DocumentBusinessLogic.cs
+++ DocumentsManager/BusinessLogic/DocumentBusinessLogic.cs
@@ -17,27 +17,50 @@ namespace DocumentsManager.BusinessLogic
                 chart.AddValueToDate(item);
             }
         }
+        private void LoadFooter(Document document)
+        {
+            FooterBusinessLogic footerLogic = new FooterBusinessLogic();
+            document.Footer = footerLogic.GetById(document.Footer.Id);
+        }
+        private void LoadFormat(Document document)
+        {
+            FormatBusinessLogic formatLogic = new FormatBusinessLogic();
+            document.Format = formatLogic.GetById(document.Format.Id);
+        }
+        private void LoadHeader(Document document)
+        {
+            HeaderBusinessLogic headerLogic = new HeaderBusinessLogic();
+            document.Header = headerLogic.GetById(document.Header.Id);
+        }
+        private void LoadStyleClass(Document document)
+        {
+            StyleClassBusinessLogic styleClassLogic = new StyleClassBusinessLogic();
+            document.StyleClass = styleClassLogic.GetById(document.StyleClass.Id);
+        }
+        private void LoadParragraphs(Document document)
+        {
+            ParragraphBusinessLogic parragraphLogic = new ParragraphBusinessLogic();
+            List<Parragraph> parragraphs = new List<Parragraph>();
+            foreach (var item in document.Parragraphs)
+            {
+                parragraphs.Add(parragraphLogic.GetById(item.Id));
+            }
+            document.Parragraphs = parragraphs;
+        }
+        private void LoadRelatinships(Document document)
+        {
+            LoadFooter(document);
+            LoadHeader(document);
+            LoadFormat(document);
+            LoadStyleClass(document);
+            LoadParragraphs(document);
+        }
         public Document GetDocumentById(Guid id)
         {
             DocumentContext context = new DocumentContext();
             Document documentFromBD = context.GetById(id);
-            FooterBusinessLogic footerLogic = new FooterBusinessLogic();
-            documentFromBD.Footer = footerLogic.GetById(documentFromBD.Footer.Id);
-            HeaderBusinessLogic headerLogic = new HeaderBusinessLogic();
-            documentFromBD.Header = headerLogic.GetById(documentFromBD.Header.Id);
-            FormatBusinessLogic formatLogic = new FormatBusinessLogic();
-            documentFromBD.Format = formatLogic.GetById(documentFromBD.Format.Id);
-            StyleClassBusinessLogic styleClassLogic = new StyleClassBusinessLogic();
-            documentFromBD.StyleClass = styleClassLogic.GetById(documentFromBD.StyleClass.Id);
-            ParragraphBusinessLogic parragraphLogic = new ParragraphBusinessLogic();
-            List<Parragraph> parragraphs = new List<Parragraph>();
-            foreach (var item in documentFromBD.Parragraphs)
-            {
-                parragraphs.Add(parragraphLogic.GetById(item.Id));
-            }
-            documentFromBD.Parragraphs = parragraphs;
+            LoadRelatinships(documentFromBD);
             return documentFromBD;
-
         }
         public ChartIntDate GetChartFromDocument(List<DateTime> allDates, DateTime since, DateTime until)
         {
