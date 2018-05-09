8e58e2bec8b204cc101c8041605daefa479aed88
Fabian Grobert <fabian.grobert@gmail.com>
2018-05-09 02:14:26 -0300

    [GREEN] GetAllAdmins AdminBusinessLogic

---
+++ DocumentsManager/BusinessLogic/AdminBusinessLogic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentsMangerEntities;
+using DocumentsManager.Data.DA.Handler;
+
+namespace DocumentsManager.BusinessLogic
+{
+    public class AdminBusinessLogic : IAdminsBusinessLogic
+    {
+        public Guid Add(AdminUser admin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<AdminUser> GetAllAdmins()
+        {
+            UserContext uContext = new UserContext();
+            return uContext.GetAdmins();
+        }
+
+        public AdminUser GetByID(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Guid id, AdminUser newAdmin)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DocumentsManager/DocumentsManager.BusinessLogic.Tests/AdminBusinessLogicTest.cs
+++ DocumentsManager/DocumentsManager.BusinessLogic.Tests/AdminBusinessLogicTest.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using DocumentsManager.Data.DA.Handler;
+using DocumentsManagerDATesting;
+using DocumentsManagerExampleInstances;
+using DocumentsMangerEntities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,49 @@ using System.Threading.Tasks;
 
 namespace DocumentsManager.BusinessLogic.Tests
 {
-    class AdminBusinessLogicTest
+    [TestClass]
+    public class AdminBusinessLogicTest
     {
+        public void TearDown()
+        {
+            ClearDataBase.ClearAll();
+        }
+        [TestMethod]
+        public void GetAdminsTest()
+        {
+            AdminBusinessLogic adminBL = new AdminBusinessLogic();
+            UserContext uContext = new UserContext();
+            AdminUser anAdmin = EntitiesExampleInstances.TestAdminUser();
+            uContext.Add(anAdmin);
+            bool expectedResult = true;
+            bool result = adminBL.GetAllAdmins().Contains(anAdmin);
+            Assert.AreEqual(expectedResult, result);
+            TearDown();
+        }
+        [TestMethod]
+        public void GetAdminsTestSeveral()
+        {
+            AdminBusinessLogic adminBL = new AdminBusinessLogic();
+            UserContext uContext = new UserContext();
+            AdminUser anAdmin = EntitiesExampleInstances.TestAdminUser();
+            AdminUser anotherAdmin = EntitiesExampleInstances.TestAdminUser();
+            uContext.Add(anAdmin);
+            uContext.Add(anotherAdmin);
+            bool expectedResult = true;
+            IEnumerable<AdminUser> allAdmins = adminBL.GetAllAdmins();
+            bool resultOne = allAdmins.Contains(anAdmin);
+            bool resultTwo = allAdmins.Contains(anotherAdmin);
+            Assert.AreEqual(expectedResult, resultOne && resultTwo);
+            TearDown();
+        }
+        [TestMethod]
+        public void GetAdminsTestEmpty()
+        {
+            AdminBusinessLogic adminBL = new AdminBusinessLogic();
+            bool expectedResult = true;
+            bool result = adminBL.GetAllAdmins().Count() == 0;
+            Assert.AreEqual(expectedResult, result);
+            TearDown();
+        }
     }
 }
