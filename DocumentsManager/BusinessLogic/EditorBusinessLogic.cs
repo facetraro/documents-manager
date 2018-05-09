d78fc43093f5ae869a56bde256ff671d6dfd41f1
Fabian Grobert <fabian.grobert@gmail.com>
2018-05-09 01:47:24 -0300

    [GREEN] GetAllEditors EditorBusinessLogic

---
+++ DocumentsManager/BusinessLogic/EditorBusinessLogic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentsMangerEntities;
+using DocumentsManager.Data.DA.Handler;
+
+namespace DocumentsManager.BusinessLogic
+{
+    public class EditorBusinessLogic : IEditorsBusinessLogic
+    {
+        public Guid Add(EditorUser editor)
+        {
+            UserContext uContext = new UserContext();
+            uContext.Add(editor);
+            return editor.Id;
+        }
+
+        public bool Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<EditorUser> GetAllEditors()
+        {
+            UserContext uContext = new UserContext();
+            return uContext.GetEditors();
+        }
+
+        public EditorUser GetByID(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Guid id, EditorUser newEditor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
