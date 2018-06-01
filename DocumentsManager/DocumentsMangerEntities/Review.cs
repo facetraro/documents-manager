89c5e85dc51a49e4c512cd8ec340ece191883d6f
Federico Cetraro <facetraro@gmail.com>
2018-06-01 16:52:54 -0300

    [GREEN] ReviewTest

---
+++ DocumentsManager/DocumentsMangerEntities/Review.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentsMangerEntities
+{
+    public class Review
+    {
+        public User Commentator { get; set; }
+        public Document Commented { get; set; }
+        public int Rating { get; set; }
+        public string FeedBack { get; set; }
+        public Guid Id { get; set; }
+    }
+}
