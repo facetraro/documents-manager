171bb4ead9174f8711c5432def09f0bec86849f7
Federico Cetraro <facetraro@gmail.com>
2018-06-04 15:48:22 -0300

    Se crea ventana inicial

---
+++ DocumentsManager/DocumentsManager.WinApp/Controls/Login.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DocumentsManager.WinApp
+{
+    public partial class LoginControl : UserControl
+    {
+        private Panel mainPanel;
+        public LoginControl(Panel panel)
+        {
+            mainPanel = panel;
+            InitializeComponent();
+            textBoxPassword.PasswordChar = '*';
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DocumentsManager/DocumentsManager.WinApp/PrincipalWindows.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DocumentsManager.WinApp
+{
+    public partial class DocumentsManager : Form
+    {
+        public DocumentsManager()
+        {
+            InitializeComponent();
+            mainPanel.Controls.Clear();
+            LoginControl theSchoolVanMenu = new LoginControl(mainPanel);
+            mainPanel.Controls.Add(theSchoolVanMenu);
+        }
+    }
+}
