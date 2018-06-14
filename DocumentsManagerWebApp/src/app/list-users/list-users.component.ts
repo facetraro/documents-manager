e0cc7b39bb8eb882fd2e54b05113e7d01e3be376
Federico Cetraro <facetraro@gmail.com>
2018-06-14 19:58:24 -0300

    Se mejora diseño de lista de usuarios y se crean metodos para comenzar a realizar operaciones de estos

--- DocumentsManagerWebApp/src/app/list-users/list-users.component.ts
+++ DocumentsManagerWebApp/src/app/list-users/list-users.component.ts
@@ -10,6 +10,7 @@ import { ManageToken } from '../manage-token';
   styleUrls: ['./list-users.component.css']
 })
 export class ListUsersComponent {
+  addFriendImgPath: string;
   pageTitle: string = "User List";
   listFilter: string = "";
   users: Array<FullUserData> = [];
@@ -18,16 +19,26 @@ export class ListUsersComponent {
 
   constructor( private service : GetAllUsersService, private router: Router) {
     this.tokenManagment=new ManageToken;
+    this.addFriendImgPath = '/media/add.png';
     this.activeToken=this.tokenManagment.getToken();
     this.loadUsers(this.activeToken);
   }
   showErrorMessage(error:any){
     alert(error);
-}
- loadUsers(token: string ){
+  }
+  loadUsers(token: string ){
     this.service.getAllUsers(token).subscribe(response => this.users=response), 
       error => this.showErrorMessage(error);
-}
+  }
+  addFriendUser(id:string){
+    console.log(id);
+  }
+  modifyUser(id:string){
+    console.log(id);
+  }
+  deleteUser(id:string){
+    console.log(id);
+  }
 }
 
 
