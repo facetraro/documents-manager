71d14dff44f87e342cd19f45f626e076cf015d0a
Federico Cetraro <facetraro@gmail.com>
2018-06-15 19:23:08 -0300

    Se mejora la interfaz del add new admin

--- DocumentsManagerWebApp/src/app/new-admin/new-admin.component.ts
+++ DocumentsManagerWebApp/src/app/new-admin/new-admin.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FullUserData } from 'src/app/list-admin/FullUserData';
 import { AdminService } from '../list-admin/admin.service';
 import { ManageToken } from 'src/app/manage-token';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-admin',
@@ -11,21 +12,29 @@ import { ManageToken } from 'src/app/manage-token';
 export class NewAdminComponent implements OnInit {
   user:FullUserData;
   tokenManagment:ManageToken;
-  constructor(private adminService : AdminService) { 
+  constructor(
+    private adminService : AdminService,
+    private router: Router
+  ) { 
     this.tokenManagment=new ManageToken;
     this.user=new FullUserData("","","","","","");
   }
 
   ngOnInit() {
   }
-  
+
   showErrorMessage(error:any){
     alert(error);
   }
 
+  userAddedOk(){
+    alert("Usuario añadido correctamente");
+    this.router.navigate((['/admins']));
+  }
+
   addNewUser(){
     let token=this.tokenManagment.getToken();
-    this.adminService.newAdmin(token, this.user).subscribe(response => window.location.reload()), 
-    error => this.showErrorMessage(error);
+    this.adminService.newAdmin(token, this.user).subscribe(response => this.userAddedOk(), 
+    error => this.showErrorMessage(error));
   }
 }
