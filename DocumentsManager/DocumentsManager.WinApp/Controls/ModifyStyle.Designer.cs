66a541fa4272afb38c64e58cf4c41353f20ca6a1
Federico Cetraro <facetraro@gmail.com>
2018-06-21 14:32:26 -0300

    Se incluye based en la UI de la Win Api

--- DocumentsManager/DocumentsManager.WinApp/Controls/ModifyStyle.Designer.cs
+++ DocumentsManager/DocumentsManager.WinApp/Controls/ModifyStyle.Designer.cs
@@ -41,12 +41,15 @@
             this.buttonBack = new System.Windows.Forms.Button();
             this.buttonCreateFormat = new System.Windows.Forms.Button();
             this.comboBoxAttributeValue = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBoxBasedOn = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
             this.SuspendLayout();
             // 
             // numericUpDown
             // 
-            this.numericUpDown.Location = new System.Drawing.Point(138, 160);
+            this.numericUpDown.Location = new System.Drawing.Point(138, 184);
             this.numericUpDown.Minimum = new decimal(new int[] {
             2,
             0,
@@ -64,7 +67,7 @@
             // comboBox
             // 
             this.comboBox.FormattingEnabled = true;
-            this.comboBox.Location = new System.Drawing.Point(138, 106);
+            this.comboBox.Location = new System.Drawing.Point(138, 142);
             this.comboBox.Name = "comboBox";
             this.comboBox.Size = new System.Drawing.Size(443, 21);
             this.comboBox.TabIndex = 25;
@@ -139,7 +142,7 @@
             // labelStylesToAdd1
             // 
             this.labelStylesToAdd1.AutoSize = true;
-            this.labelStylesToAdd1.Location = new System.Drawing.Point(26, 109);
+            this.labelStylesToAdd1.Location = new System.Drawing.Point(26, 148);
             this.labelStylesToAdd1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelStylesToAdd1.Name = "labelStylesToAdd1";
             this.labelStylesToAdd1.Size = new System.Drawing.Size(79, 13);
@@ -178,23 +181,58 @@
             this.buttonCreateFormat.Name = "buttonCreateFormat";
             this.buttonCreateFormat.Size = new System.Drawing.Size(63, 28);
             this.buttonCreateFormat.TabIndex = 15;
-            this.buttonCreateFormat.Text = "Crear";
+            this.buttonCreateFormat.Text = "Modificar";
             this.buttonCreateFormat.UseVisualStyleBackColor = false;
             this.buttonCreateFormat.Click += new System.EventHandler(this.buttonCreateFormat_Click);
             // 
             // comboBoxAttributeValue
             // 
             this.comboBoxAttributeValue.FormattingEnabled = true;
-            this.comboBoxAttributeValue.Location = new System.Drawing.Point(138, 162);
+            this.comboBoxAttributeValue.Location = new System.Drawing.Point(138, 183);
             this.comboBoxAttributeValue.Name = "comboBoxAttributeValue";
             this.comboBoxAttributeValue.Size = new System.Drawing.Size(443, 21);
             this.comboBoxAttributeValue.TabIndex = 27;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(511, 93);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 28);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "Limpiar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBoxBasedOn
+            // 
+            this.comboBoxBasedOn.FormattingEnabled = true;
+            this.comboBoxBasedOn.Location = new System.Drawing.Point(138, 100);
+            this.comboBoxBasedOn.Name = "comboBoxBasedOn";
+            this.comboBoxBasedOn.Size = new System.Drawing.Size(368, 21);
+            this.comboBoxBasedOn.TabIndex = 29;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 103);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Basado en:";
             // 
             // ModifyStyle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBoxBasedOn);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBoxAttributeValue);
             this.Controls.Add(this.numericUpDown);
             this.Controls.Add(this.comboBox);
@@ -231,5 +269,8 @@
         private System.Windows.Forms.Button buttonBack;
         private System.Windows.Forms.Button buttonCreateFormat;
         private System.Windows.Forms.ComboBox comboBoxAttributeValue;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBoxBasedOn;
+        private System.Windows.Forms.Label label1;
     }
 }
