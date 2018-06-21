9f2b5154d983dc818a9c9ff1b7e4841989102432
Federico Cetraro <facetraro@gmail.com>
2018-06-21 15:15:18 -0300

    Se añade boton para cerrar WinApp

--- DocumentsManager/DocumentsManager.WinApp/PrincipalWindows.Designer.cs
+++ DocumentsManager/DocumentsManager.WinApp/PrincipalWindows.Designer.cs
@@ -30,36 +30,62 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DocumentsManager));
             this.mainPanel = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.jkjikbToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
             this.mainPanel.BackgroundImage = global::DocumentsManager.WinApp.Properties.Resources.documents;
             this.mainPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.mainPanel.Location = new System.Drawing.Point(0, 2);
+            this.mainPanel.Location = new System.Drawing.Point(1, 27);
             this.mainPanel.Name = "mainPanel";
             this.mainPanel.Size = new System.Drawing.Size(627, 462);
             this.mainPanel.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.jkjikbToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(628, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // jkjikbToolStripMenuItem
+            // 
+            this.jkjikbToolStripMenuItem.Image = global::DocumentsManager.WinApp.Properties.Resources._012_power_512;
+            this.jkjikbToolStripMenuItem.Name = "jkjikbToolStripMenuItem";
+            this.jkjikbToolStripMenuItem.Size = new System.Drawing.Size(28, 20);
+            this.jkjikbToolStripMenuItem.Click += new System.EventHandler(this.jkjikbToolStripMenuItem_Click);
             // 
             // DocumentsManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
-            this.ClientSize = new System.Drawing.Size(628, 466);
+            this.ClientSize = new System.Drawing.Size(628, 488);
             this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "DocumentsManager";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Documents Manager";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem jkjikbToolStripMenuItem;
     }
 }
 
